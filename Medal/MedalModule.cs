7be3c859682162425da9dfb45536070deca3c3ae
LucHeart <luc@luc.cat>
2025-07-12 23:28:08 +0200

    Initial commit

---
+++ Medal/MedalModule.cs
@@ -0,0 +1,11 @@
+﻿using OpenShock.Desktop.ModuleBase;
+using OpenShock.Desktop.ModuleBase.Navigation;
+using OpenShock.Desktop.Modules.Medal;
+
+[assembly:DesktopModule(typeof(MedalModule), "openshock.desktop.modules.medal", "Medal ICYMI")]
+namespace OpenShock.Desktop.Modules.Medal;
+
+public sealed class MedalModule : DesktopModuleBase
+{
+    public override IReadOnlyCollection<NavigationItem> NavigationComponents { get; }
+}
